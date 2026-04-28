7fddb777cf3c3fc914ac7dc15c6523b557295ced
Alvaro Flores <f.alvaro.ro@gmail.com>
2026-04-28 13:56:18 -0400

    chore: configure Vite plugins for compression and image optimization

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteCompression from 'vite-plugin-compression'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+    }),
+    ViteImageOptimizer({
+      webp: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      png: {
+        quality: 80,
+      },
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'react-icons': ['react-icons/fa', 'react-icons/fa6', 'react-icons/fi', 'react-icons/hi'],
+        }
+      }
+    }
+  }
 })
